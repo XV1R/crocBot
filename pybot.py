928c7cbb349104fc4ffa04552378444b7f92f0ff
XV1R <xavytron@gmail.com>
2020-04-06 17:14:14 -0400

    Now implemented basic guessing game for crocBot

--- pybot.py
+++ pybot.py
@@ -1,5 +1,8 @@
 import discord
+import random
+import asyncio
 
+#the bot instance
 client = discord.Client()
 
 @client.event
@@ -8,8 +11,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    #checks if the message was sent by the person who 
-    # called the bot
+    #makes sure the bot doesn't respond to itself
     if message.author == client.user:
         return
         
@@ -19,6 +21,25 @@ async def on_message(message):
     if message.content.startswith('$ping'):
         await message.channel.send('pong')
 
+    if message.content.startswith('$guess'):
+        await message.channel.send('Guess a number between 1 and 10.')
+    
+        def is_correct(m):
+            return m.author == message.author and m.content.isdigit()
+
+        answer = random.randint(1,10)
+        print(answer)
+
+        try:
+            #waits 10 seconds for a response 
+            guess = await client.wait_for('message', check=is_correct, timeout=10.0)
+        except asyncio.TimeoutError:
+            return await message.channel.send('Sorry, you took too long. It was {}.'.format(answer))
+        
+        if int(guess.content) == answer:
+            await message.channel.send("You're right! The number was {}".format(answer))
+        else:
+            await message.channel.send("Oops. It's actually {}".format(answer))
 
 #DO NOT CHANGE
 client.run("Njk2Nzg4Mjk4MzMyNTA0MTE1.Xot5Cg.3mO0yfxjb5cBJAmVyGLvHT1cpDM")
